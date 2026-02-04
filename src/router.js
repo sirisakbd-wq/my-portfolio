0ae16e56edcc9939a21eb070a151e007e1f3a4e4
sirisakbd-wq <sirisak.bd@gmail.com>
2026-02-04 16:27:09 +0700

    first commit

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import PortfolioView from './views/PortfolioView.vue'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   { path: '/', component: HomeView },
   { path: '/portfolio', component: PortfolioView }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // ใส่ชื่อ Repository ที่คุณจะสร้างใน GitHub
+  base: '/my-portfolio/', 
   plugins: [
     vue(),
     vueDevTools(),
